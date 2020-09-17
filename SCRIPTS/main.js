7cc0dbfecdbd56f0c38b46f4bbb33c5202fa6d77
Polina <Chzhan>
2020-09-16 19:37:21 -0700

    cards layout home

--- SCRIPTS/main.js
+++ SCRIPTS/main.js
@@ -3,8 +3,8 @@
                   dots: true,
                   infinite: false,
                   speed: 300,
-                  slidesToShow: 4,
-                  slidesToScroll: 4,
+                  slidesToShow: 3,
+                  slidesToScroll: 1,
                   autoplay: false,
                   autoplaySpeed: 2000,
                   infinite: true,
@@ -13,7 +13,7 @@
                       breakpoint: 1024,
                       settings: {
                         slidesToShow: 3,
-                        slidesToScroll: 3,
+                        slidesToScroll: 1,
                         infinite: true,
                         dots: true
                       }
@@ -22,14 +22,16 @@
                       breakpoint: 600,
                       settings: {
                         slidesToShow: 2,
-                        slidesToScroll: 2
+                        slidesToScroll: 1,
+                        dots: true
                       }
                     },
                     {
                       breakpoint: 480,
                       settings: {
                         slidesToShow: 1,
-                        slidesToScroll: 1
+                        slidesToScroll: 1,
+                        dots: true
                       }
                     }
                   ]
