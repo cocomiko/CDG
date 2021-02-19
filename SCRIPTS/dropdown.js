86c26c11f6fa377e58b762277e283f3abef2d927
Polina <Chzhan>
2021-02-18 22:00:36 -0800

    Css & JS code changed to minified

--- SCRIPTS/dropdown.js
+++ SCRIPTS/dropdown.js
@@ -1,11 +1,1 @@
-
-  $('.dropdownn').click(function(e){
-    $(this).find('.dropdownn-menu').toggleClass('open');
-    $($(e.target).find('.down-caret').toggleClass('open-caret'));
-    e.preventDefault();
-    e.stopPropagation();
-    $(document).click(function(){
-      $('.dropdownn-menu').removeClass('open');
-      $('.down-caret').removeClass('open-caret');
-    });
-  });
+$(".dropdownn").click(function(n){$(this).find(".dropdownn-menu").toggleClass("open"),$($(n.target).find(".down-caret").toggleClass("open-caret")),n.preventDefault(),n.stopPropagation(),$(document).click(function(){$(".dropdownn-menu").removeClass("open"),$(".down-caret").removeClass("open-caret")})});
--- SCRIPTS/filterprojectsbytopic.js
+++ SCRIPTS/filterprojectsbytopic.js
@@ -1,17 +1,1 @@
-var $projcards = $('.projcards').isotope({
-  itemSelector: '.item',
-  layoutMode: 'fitRows'
-});
-
-$('.filters-button-group').on( 'click', 'button', function() {
-  var filterValue = $( this ).attr('data-filter');
-  $projcards.isotope({ filter: filterValue });
-});
-
-$('.btn-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', 'button', function() {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
-  });
-});
+var $projcards=$(".projcards").isotope({itemSelector:".item",layoutMode:"fitRows"});$(".filters-button-group").on("click","button",function(){var t=$(this).attr("data-filter");$projcards.isotope({filter:t})}),$(".btn-group").each(function(t,o){var e=$(o);e.on("click","button",function(){e.find(".is-checked").removeClass("is-checked"),$(this).addClass("is-checked")})});
